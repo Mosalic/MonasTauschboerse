27ac688283ec33ed10080c9dbcced67eecb9b601
Röttger <mona.roettger@gmail.com>
2017-08-15 14:32:40 +0200

    add der right TestIMP unityproject

---
+++ TestIMP/Assets/Scripts/Sporner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sporner : MonoBehaviour {
+
+    public GameObject spawnObj;
+   
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("InstantiateMarble")) {
+            Debug.Log("Kugel erstellen");
+            
+
+            /*var mousePos = Input.mousePosition;
+            mousePos.z = 2;       // we want 2m away from the camera position
+
+            var objectPos = Camera.current.ScreenToWorldPoint(mousePos);
+
+            var myObject = Instantiate(spawnObj, objectPos, Quaternion.identity);*/
+        }
+	}
+}
